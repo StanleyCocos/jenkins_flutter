cc46a4cbdf785211c5a77c616c019c98b703fbed
10456 <exscall@addcn.com>
2023-12-08 13:58:16 +0800

    测试6

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '测试 4git hook',
+              '测试 5 git hook',
             ),
             Text(
               '测试',
